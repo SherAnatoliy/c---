fbddbaf432db9ca36e89d4da93e6784c70245535
SherAnatoliy <164882963+SherAnatoliy@users.noreply.github.com>
2024-03-28 11:45:41 +0500

    Update6

---
+++ JavaScript/JavsScript.js
@@ -0,0 +1,31 @@
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function nextSlide() {
+    showSlide(slideIndex += 1);
+}
+
+function prevSlide() {
+    showSlide(slideIndex -= 1);
+}
+
+function currentSlide(n) {
+    showSlide(slideIndex = n - 1);
+}
+
+function showSlide(n) {
+    const slides = document.getElementsByClassName("slide");
+    const radios = document.getElementsByName("slider-radio");
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.transform = "translateX(-" + (slideIndex * 100) + "%)";
+    }
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].checked = (i === slideIndex);
+    }
+}
