9548f489d467848a29773deb57ff6a2eed87ad58
SherAnatoliy <164882963+SherAnatoliy@users.noreply.github.com>
2024-03-29 16:38:31 +0500

    update15

--- JavaScript/JavsScript.js
+++ JavaScript/JavsScript.js
@@ -1,38 +1,38 @@
 
 
-let slideIndex = 0;
-showSlide(slideIndex);
+let slideIndex = 0
+showSlide(slideIndex)
 
 
 
 
 
 function nextSlide() {
-    showSlide(slideIndex += 1);
+    showSlide(slideIndex += 1)
 }
 
 function prevSlide() {
-    showSlide(slideIndex -= 1);
+    showSlide(slideIndex -= 1)
 }
 
 function currentSlide(n) {
-    showSlide(slideIndex = n - 1);
+    showSlide(slideIndex = n - 1)
 }
 
 function showSlide(n) {
-    const slides = document.getElementsByClassName("slide");
-    const radios = document.getElementsByName("slider-radio");
+    const slides = document.getElementsByClassName("slide")
+    const radios = document.getElementsByName("slider-radio")
     if (n >= slides.length) {
-        slideIndex = 0;
+        slideIndex = 0
     }
     if (n < 0) {
-        slideIndex = slides.length - 1;
+        slideIndex = slides.length - 1
     }
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.transform = "translateX(-" + (slideIndex * 100) + "%)";
+        slides[i].style.transform = "translateX(-" + (slideIndex * 100) + "%)"
     }
     for (let i = 0; i < radios.length; i++) {
-        radios[i].checked = (i === slideIndex);
+        radios[i].checked = (i === slideIndex)
     }
 }
 
