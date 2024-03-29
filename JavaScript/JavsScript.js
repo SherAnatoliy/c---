f61e0428af7cb600dd6dd118372f12d7b951e32d
SherAnatoliy <164882963+SherAnatoliy@users.noreply.github.com>
2024-03-30 03:10:50 +0500

    end3

--- JavaScript/JavsScript.js
+++ JavaScript/JavsScript.js
@@ -1,4 +1,16 @@
+// Start number
+function changeNumber() {
+    var select = document.getElementById("NumbergeSelect");
+    var telNumber = document.getElementById("telNumber");
 
+    if (select.value === "en") {
+        telNumber.textContent = "TEL 646-791-3726";
+    } else if (select.value === "ru") {
+        telNumber.textContent = "TEL 8800-2000-206";
+    }
+}
+// EnD
+// Slider start
 let slideIndex = 0
 showSlide(slideIndex)
 function nextSlide() {
@@ -28,7 +40,23 @@ function showSlide(n) {
         radios[i].checked = (i === slideIndex)
     }
 }
+// slider End
+// Slider2 start
+let slideIndex2 = 0;
+const slides = document.querySelectorAll('.slide2');
 
+function showSlides2() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.transform = `translateX(-${slideIndex2 * 100}%)`;
+    }
+    slideIndex2++;
+    if (slideIndex2 >= slides.length) {
+        slideIndex2 = 0;
+    }
+}
+
+setInterval(showSlides2, 3000);
+// Slider2 end
 // PopUp form
 let whiteBtn = document.getElementsByClassName("section9_button_text")[0]
 let popUpWrapper = document.querySelector(".popUpForm")
