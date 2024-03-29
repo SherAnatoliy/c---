1dc2af0da07b5ce6c6701f02f50b4a6b4d250017
SherAnatoliy <164882963+SherAnatoliy@users.noreply.github.com>
2024-03-30 02:19:25 +0500

    update fimish

--- JavaScript/JavsScript.js
+++ JavaScript/JavsScript.js
@@ -1,12 +1,6 @@
 
-
 let slideIndex = 0
 showSlide(slideIndex)
-
-
-
-
-
 function nextSlide() {
     showSlide(slideIndex += 1)
 }
@@ -18,7 +12,6 @@ function prevSlide() {
 function currentSlide(n) {
     showSlide(slideIndex = n - 1)
 }
-
 function showSlide(n) {
     const slides = document.getElementsByClassName("slide")
     const radios = document.getElementsByName("slider-radio")
@@ -36,12 +29,43 @@ function showSlide(n) {
     }
 }
 
+// PopUp form
+let whiteBtn = document.getElementsByClassName("section9_button_text")[0]
+let popUpWrapper = document.querySelector(".popUpForm")
+let closeBtn = document.querySelector(".close")
+
+whiteBtn.addEventListener("click" , popUpForm);
+closeBtn.addEventListener("click" , closePopUp)
+function popUpForm () { 
+   popUpWrapper.style.display = "flex";
+}
 
 
+function closePopUp(){
+    popUpWrapper.style.display = "none";
+}
 
 
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide' );
+    splide.mount();
+  } );
 
 
+// End
+
+function checkConfirm(){
+    let confirm = document.getElementsByName("confirm")[0].checked
+    let sendBtn = document.getElementsByName("sendBtn")[0]
+
+    console.log(confirm)
+    if (confirm == true) {
+       
+        sendBtn.removeAttribute("disabled") 
+    } else {
+        sendBtn.setAttribute("disabled" , "disabled")
+    }
+}
 
 
 
